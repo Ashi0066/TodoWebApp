6430d1498f5847abf1936261e085292209ee4ac2
ashir7171 <u2213256@uel.ac.uk>
2024-11-04 02:48:40 +0000

    Starter

--- src/main/java/com/webapp/springboot/TodoWebApp/Repository/TodoRepository.java
+++ src/main/java/com/webapp/springboot/TodoWebApp/Repository/TodoRepository.java
@@ -1,9 +1,15 @@
 package com.webapp.springboot.TodoWebApp.Repository;
 
 import com.webapp.springboot.TodoWebApp.Todo.TodoLogic;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+
+@Transactional
+@Repository
 
 public interface TodoRepository extends JpaRepository<TodoLogic,Integer>
 {
